d047c4afdec687621c1c26d29f502073f64d23ec
orianajpp <orianajpp@gmail.com>
2023-05-06 12:51:28 -0400

    prueba de pull

--- src/data.js
+++ src/data.js
@@ -1,9 +1,23 @@
 // estas funciones son de ejemplo
-
+/*
 export const example = () => {
   return 'example';
 };
 
 export const anotherExample = () => {
   return 'OMG';
-};
+};*/
+
+/*import data from './data/pokemon/pokemon.js';
+console.log(data);
+
+filterData: function  (data, conditon){
+
+  const data = pokemon.name(data);
+  pokemon.forEach(element => {
+    
+  });
+
+}*/
+
+
