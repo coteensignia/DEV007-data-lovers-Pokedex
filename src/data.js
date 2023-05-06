0a9197c2f2cc2b540c3521a237f672c213f1d7ea
coteensignia <mjensignia@gmail.com>
2023-05-06 12:17:47 -0400

    veamos

--- src/data.js
+++ src/data.js
@@ -4,6 +4,25 @@ export const example = () => {
   return 'example';
 };
 
+
+
+
 export const anotherExample = () => {
   return 'OMG';
 };
+
+
+
+export const pruebaCote = () => {
+  fetch ("./data/pokemon/pokemon.json")
+  .then((response) => response.json())
+  .then((json) => {
+    console.log( "json")
+  })
+    
+
+
+};
+
+
+
