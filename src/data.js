22fdefc28745c6e2c1d9cf8876be4145eeb65150
orianajpp <orianajpp@gmail.com>
2023-05-16 07:14:17 -0400

    agrega filtro por nombre y paginado

--- src/data.js
+++ src/data.js
@@ -1,12 +1,9 @@
 // estas funciones son de ejemplo
+
+export 
+
+
 /*
-export const example = () => {
-  return 'example';
-};
-
-
-
-
 export const anotherExample = () => {
   return 'OMG';
 };
--- src/main.js
+++ src/main.js
@@ -1,188 +1,201 @@
 //import { name, type } from './data.js';
 // import data from './data/lol/lol.js';
 //import pokemon from './data/pokemon/pokemon.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-
-// crear una funcion para obtener los pokemons
-//la funcion debe ir al objeto pokemon
-//recorrerlo con un for o foreach para obtener numero,nombre, imagen y tipo
-    
+import data from "./data/pokemon/pokemon.js";
+//llamasdos al DOM
 const containerCard = document.querySelector(".containerCard");
 const infoCard = document.getElementById("infoCard");
-const upperDoc = document.getElementById("upperDoc");
-const bottomDoc = document.getElementById("bottomDoc");
 
-
-/*const card = document.querySelectorAll(".card");*/
-// Creación de objeto de type
-const x ={ 
-  water: '',
-  dragon: 'https://images.wikidexcdn.net/mwuploads/wikidex/1/15/latest/20230128124905/Tipo_drag%C3%B3n_icono_EP.svg',
-  electric: 'https://images.wikidexcdn.net/mwuploads/wikidex/8/84/latest/20230128125008/Tipo_el%C3%A9ctrico_icono_EP.svg',
-  "fairy":'https://images.wikidexcdn.net/mwuploads/wikidex/b/b7/latest/20230128125233/Tipo_hada_icono_EP.svg',
-  ghosh:'https://images.wikidexcdn.net/mwuploads/wikidex/3/3d/latest/20230128125103/Tipo_fantasma_icono_EP.svg',
-  fire:'https://images.wikidexcdn.net/mwuploads/wikidex/5/55/latest/20230128125153/Tipo_fuego_icono_EP.svg',
-  ice:'https://images.wikidexcdn.net/mwuploads/wikidex/a/a6/latest/20230128125423/Tipo_hielo_icono_EP.svg',
-  grass: 'https://images.wikidexcdn.net/mwuploads/wikidex/e/ed/latest/20230128125654/Tipo_planta_icono_EP.svg',
-  bug: 'https://images.wikidexcdn.net/mwuploads/wikidex/1/1a/latest/20230128124809/Tipo_bicho_icono_EP.svg',
-  fighting: 'https://images.wikidexcdn.net/mwuploads/wikidex/f/f2/latest/20230128125518/Tipo_lucha_icono_EP.svg',
-  normal: 'https://images.wikidexcdn.net/mwuploads/wikidex/c/c3/latest/20230128125621/Tipo_normal_icono_EP.svg',
-  dark: 'https://images.wikidexcdn.net/mwuploads/wikidex/e/e0/latest/20230128132504/Tipo_siniestro_icono_EP.svg',
-  steel: 'https://images.wikidexcdn.net/mwuploads/wikidex/6/6c/latest/20230128124521/Tipo_acero_icono_EP.svg',
-  rock: 'https://images.wikidexcdn.net/mwuploads/wikidex/1/14/latest/20230128125805/Tipo_roca_icono_EP.svg',
-  psychic: 'https://images.wikidexcdn.net/mwuploads/wikidex/2/22/latest/20230128125735/Tipo_ps%C3%ADquico_icono_EP.svg',
-  ground: 'https://images.wikidexcdn.net/mwuploads/wikidex/c/c8/latest/20230128132625/Tipo_tierra_icono_EP.svg',
-  poison: 'https://images.wikidexcdn.net/mwuploads/wikidex/f/fa/latest/20230128132735/Tipo_veneno_icono_EP.svg',
-  fly: 'https://images.wikidexcdn.net/mwuploads/wikidex/6/6b/latest/20230128132815/Tipo_volador_icono_EP.svg'
-
-};
-
-function getTypePokemon(types){
-  let img ="";
-  types.forEach(function(type){
-    console.log(type);
-    
-    img+= `<img src="./img/${type}.png" alt="${type}" widht="50px" height="50px">`;
-   /* switch(type)
-    {
-      case 'water': 
-       
-      break; 
-      case 'dragon': break;
-      case 'electric': break;
-      case 'ghosh': break;
-      case 'fire': break;
-      case 'ice': break;
-      case 'grass': break;
-      case 'bug': break;
-      case 'electric': break;
-      case 'fighting': break;
-      case 'normal': break;
-      case 'dark': break;
-      case 'steel': break;
-      case 'rock': break;
-      case 'psychic': break;
-      case 'ground': break;
-      case 'poison': break;
-      case 'fly': break;
-      default: break;
-
-
-    }*/
-    
-
-  }  
-  );
+//creacion de funcion para recorrer
+function getTypePokemon(types) {
+  let img = "";
+  types.forEach(function (type) {
+    img += `<img src="./img/${type}.png" alt="${type}" widht="50px" height="50px">`;
+  });
   return img;
- 
- /* if(n === "water"){return (x.water);} 
-  else if(n === "dragon"){return (x.dragon);}
-  else if(n === "electric"){return (x.electric);}
-  else if(n === "ghosh"){return (x.ghosh);}
-  else if(n === "fire"){return (x.fire);}
-  else if(n === "ice"){return (x.ice);}
-  else if(n === "grass"){return (x.grass);}
-  else if(n === "bug"){return (x.bug);}
-  else if(n === "fighting"){return (x.fighting);}
-  else if(n === "normal"){return (x.normal);}
-  else if(n === "dark"){return (x.dark);}
-  else if(n === "steel"){return (x.steel);}
-  else if(n === "rock"){return (x.rock);}
-  else if(n === "psychic"){return (x.psychic);}
-  else if(n === "ground"){return (x.ground);}
-  else if(n === "poison"){return (x.poison);}
-  else if(n === "fly"){return (x.fly);}*/
 }
-
-
-
 
 /*apaga modal*/
 infoCard.style.display = "none";
 
-data.pokemon.forEach((i,idx) => {
-  let typePokemon = getTypePokemon(i.type);
-  containerCard.innerHTML += ` 
-    <button class="card" id="${idx}">
+//impresion de cartas con paginación
+
+var current_page = 1;
+var obj_per_page = 30;
+function totNumPages() {
+  return Math.ceil(data.pokemon.length / obj_per_page);
+}
+
+function prevPage() {
+  containerCard.innerHTML = "";
+  if (current_page > 1) {
+    current_page--;
+    change(current_page);
+  }
+}
+function nextPage() {
+  containerCard.innerHTML = "";
+  if (current_page < totNumPages()) {
+    current_page++;
+    change(current_page);
+  }
+}
+function change(page) {
+  var btn_next = document.getElementById("btn_next");
+  btn_next.addEventListener("click", nextPage);
+  var btn_prev = document.getElementById("btn_prev");
+  btn_prev.addEventListener("click", prevPage);
+
+  var page_span = document.getElementById("page");
+  if (page < 1) page = 1;
+  if (page > totNumPages()) page = totNumPages();
+
+  for (var i = (page - 1) * obj_per_page; i < page * obj_per_page; i++) {
+    
+    let pokemon_unit = data.pokemon[i];
+    let typePokemon = getTypePokemon(pokemon_unit.type);
+
+    containerCard.innerHTML += ` 
+      <div class="card-pokemon"> 
+        <button class="card" id="${Math.abs(pokemon_unit.num)-1}">  
+          <div class="card-content">
+            <p class="numberPokemon" >${pokemon_unit.num}</p>
+            <h3>${pokemon_unit.name}</h3>
+            <div class="power">
+            ${typePokemon}
+            </div>
+          </div>
+          <div class="card-img">
+            <img src="${pokemon_unit.img}" class="imagen" alt=""> 
+          </div>
+        </button>
+      </div>`;
+        
+  }
+
+  for(let i = 0; i < containerCard.children.length; i++){
+    console.log(containerCard.children[i]);
+    containerCard.children[i].addEventListener("click", (e) => {
+      console.log(e.target.classList(card-pokemon))
+
+    });
+  }
+
+  page_span.innerHTML = page;
+  if (page == 1) {
+    btn_prev.style.visibility = "hidden";
+  } else {
+    btn_prev.style.visibility = "visible";
+  }
+  if (page == totNumPages()) {
+    btn_next.style.visibility = "hidden";
+    
+  } else {
+    btn_next.style.visibility = "visible";
+  }
+}
+window.onload = function () {
+  change(1);
+};
+
+
+
+function openModal(idPokemon) {
+  infoCard.style.display = "block";
+  console.log(idPokemon); 
+  const objPokemon = data.pokemon[idPokemon];
+  console.log(objPokemon);
+  document.getElementById("infoCard").innerHTML = `
+        <div class="mainInfo">
+          <p class="numberPokemon">${objPokemon.num}</p>
+          <p class="close"></p>
+          <h3>${objPokemon.name}</h3>
+          <img src="${objPokemon.img}" alt="">
+          <div>
+            <div class="power">${objPokemon.type}</div>
+            <h4>Sobre</h4>
+            <div>
+              <div class="weightDiv">
+                <div>            
+                  <img src="" alt="">
+                  <p class="weight"></p>
+                </div>
+                <p>Peso</p>
+              </div>
+              <div class="sizeDiv">
+                <div>            
+                  <img src="" alt="">
+                  <p class="size"></p>
+                </div>
+                <p>Medida</p>
+              </div>
+              <div class="movesDiv">
+                <p class="moves"></p>
+                <p>Moves</p>
+            </div>
+            <div class="description">
+              <p>${objPokemon.about}</p>
+            </div>
+            <div>
+              <h3>Estadística</h3>
+              <div>
+    
+              </div>
+            </div>
+          </div> `;         
+}
+
+
+
+document.addEventListener("keyup", e => {
+ if(e.target.matches(".search")){
+  
+    document.querySelectorAll(".card").forEach(nombre =>{
+    nombre.textContent.toLowerCase().includes(e.target.value) // incluye el valor 
+    ?nombre.classList.remove()          //remueve elementos del dom 
+    :nombre.classList.add("filtro")
+  });
+  if(e.key === "Escape")e.target.value = "";
+}
+  
+});
+
+
+/*
+const buttonType = document.getElementById("buttomType");
+const listType = document.getElementById("listType");
+
+//const iter = data.pokemon.filter(i=> type ="water")
+
+
+
+buttonType.addEventListener("click", ()=>{
+  listType.style.display = "block";
+});
+listType.addEventListener("click",(e)=>{
+const classListType = e.target.classList[0];
+switch (classListType) {
+case ('water'):  containerCard.innerHTML += ` 
+<div id="card-pokemon>
+    <button class="card" id="${Math.abs(pokemon_unit.num)-1}">
       <div class="card-content">
-        <p class="numberPokemon" >${i.num}</p>
-        <h3>${i.name}</h3>
+        <p class="numberPokemon" >${pokemon_unit.num}</p>
+        <h3>${pokemon_unit.name}</h3>
         <div class="power">
         ${typePokemon}
         </div>
       </div>
       <div class="card-img">
-        <img src="${i.img}" class="imagen" alt=""> 
+        <img src="${pokemon_unit.img}" class="imagen" alt=""> 
       </div>
-    </button>`;
-    
-});
-
-const openModal = function (idPokemon)
-{
-    infoCard.style.display = "block";
-    console.log(idPokemon);
-    const objPokemon = data.pokemon[idPokemon];
-    console.log(objPokemon);
-    infoCard.innerHTML = `
-    <div class="mainInfo">
-      <p class="numberPokemon">${objPokemon.num}</p>
-      <p class="close"></p>
-      <h3>${objPokemon.name}</h3>
-      <img src="${objPokemon.img}" alt="">
-      <div>
-        <div class="power">${objPokemon.type}</div>
-        <h4>Sobre</h4>
-        <div>
-          <div class="weightDiv">
-            <div>            
-              <img src="" alt="">
-              <p class="weight"></p>
-            </div>
-            <p>Peso</p>
-          </div>
-          <div class="sizeDiv">
-            <div>            
-              <img src="" alt="">
-              <p class="size"></p>
-            </div>
-            <p>Medida</p>
-          </div>
-          <div class="movesDiv">
-            <p class="moves"></p>
-            <p>Moves</p>
-        </div>
-        <div class="description">
-          <p>${objPokemon.about}</p>
-        </div>
-        <div>
-          <h3>Estadística</h3>
-          <div>
-
-          </div>
-        </div>
-      </div> `;
-  }
-
-
-document.addEventListener("DOMContentLoaded", function(event) {
-  console.log(event);
-  const showCard = document.getElementByClassName("card");
-console.log(showCard);
-
-  /*showCard.addEventListener("click", () =>{
-    console.log(this.id);
-    openModal(this.id);
-  });*/
-});
-
-
+    </button>
+  </div>`;
+})
   
 
+    break;
 
-
-
-
-
- 
+  default:
+    break;
+}
+});*/
