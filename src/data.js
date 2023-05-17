a0eb317561388c1002c867089773290af724827f
coteensignia <mjensignia@gmail.com>
2023-05-17 01:25:51 -0400

    agrega filtrado

--- src/data.js
+++ src/data.js
@@ -1,22 +1,149 @@
-// estas funciones son de ejemplo
 
-export 
+// card
+export function card(i, poke) {
+  const out = ` 
+  <div class="card-pokemon" id="${Math.abs(poke.num)-1}"> 
+    <button pos="${i}" class="card">  
+      <div class="card-content">
+        <p class="numberPokemon" >${poke.num}</p>
+        <h3>${poke.name}</h3>
+        <div class="power">
+        ${getTypePokemon(poke.type)}
+        </div>
+      </div>
+      <div class="card-img">
+        <img src="${poke.img}" class="imagen" alt=""> 
+      </div>
+    </button>
+  </div>`
+  return out
+}
 
 
-/*
+// card modal
+export function openCard(data_, place) {
+  document.querySelectorAll('.card').forEach((el)=>{
+    el.addEventListener('click', ()=>{
+      const pokeinfo = data_[el.getAttribute('pos')]
+
+      place.insertAdjacentHTML(
+        'beforeend', cardDetails(pokeinfo)
+      );
+      const cardDetailsContainer = document.querySelector('.card-details-container')
+      cardDetailsContainer.addEventListener('click', ()=>{
+        cardDetailsContainer.remove()
+      })
+    })
+  })
+}
+
+
+// card modal (contenido)
+function cardDetails(poke) {
+  const out  = `
+  <div class="card-details-container">
+    <div class="card-details">
+      <div class="card-details-head">
+        <div class="num">${poke.num}</div>
+        <div class="power">${getTypePokemon(poke.type)}</div>
+      </div>
+      
+      <div class="card-details-main">
+        <div class="title">${poke.name}</div>
+        <img class="image" src="${poke.img}" alt="" loading="lazy">
+      </div>
+
+      <div class="card-details-about">
+        <div class="title">Datos</div>
+        <div class="sec">
+          <div class="title">Weight</div>
+          <div class="info">${poke.size.weight}</div>
+          </div>
+          <div class="sec">
+          <div class="title">Height</div>
+          <div class="info">${poke.size.height}</div>
+        </div>
+        <div class="sec">
+          <div class="title">Rarity</div>
+          <div class="info">${poke['pokemon-rarity']}</div>
+        </div>
+        <div class="sec">
+          <div class="title">Generation</div>
+          <div class="info">${poke.generation.name}</div>
+        </div>
+      </div>
+
+      <div class="card-details-description">
+        <div class="title">Descripción</div>
+        <div class="info">${poke.about}</div>
+      </div>
+
+    </div>
+  </div>
+  `
+  return out
+}
+
+
+
+
+
+// iconos de tipo de pokemon
+function getTypePokemon(types) {
+  let img = "";
+  types.forEach(function (type) {
+    img += `<img src="./img/${type}.png" alt="${type}" widht="50px" height="50px" loading="lazy">`;
+  });
+  return img;
+}
+
+
+
+
+
+//  estas funciones son de ejemplo
+
+export const example = () => {
+  return 'example';
+};
+
 export const anotherExample = () => {
   return 'OMG';
 };
 
 
 
-export const pruebaCote = () => {
-  fetch ("./data/pokemon/pokemon.json")
-  .then((response) => response.json())
-  .then((json) => {
-    console.log( "json")
-  })
-    
 
 
-};*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,31 +1,31 @@
-//import { name, type } from './data.js';
-// import data from './data/lol/lol.js';
-//import pokemon from './data/pokemon/pokemon.js';
+
+//inicialización
+
+// important funciones para crear tarjetas "card" y crear modal "openCard"
+import { card, openCard } from './data.js';
+
+// important datos
 import data from "./data/pokemon/pokemon.js";
-//llamasdos al DOM
-const containerCard = document.querySelector(".containerCard");
-const infoCard = document.getElementById("infoCard");
 
-//creacion de funcion para recorrer
-function getTypePokemon(types) {
-  let img = "";
-  types.forEach(function (type) {
-    img += `<img src="./img/${type}.png" alt="${type}" widht="50px" height="50px">`;
-  });
-  return img;
-}
+// crear constantes cuerpo, contenedor e input de busqueda
+const body = document.querySelector("body")
+const containerCard = document.querySelector(".containerCard")
+const buscar = document.getElementById("buscar")
 
-/*apaga modal*/
-infoCard.style.display = "none";
+// crear constantes de botones para filtrado y orden
+const buttonType = document.getElementById("buttomType");
+const buttonOrder = document.getElementById("buttomOrder");
 
-//impresion de cartas con paginación
 
-var current_page = 1;
-var obj_per_page = 30;
+//impresion de cards + paginación
+let current_page = 1;
+const obj_per_page = 30;
+
 function totNumPages() {
   return Math.ceil(data.pokemon.length / obj_per_page);
 }
 
+// boton previo
 function prevPage() {
   containerCard.innerHTML = "";
   if (current_page > 1) {
@@ -33,6 +33,8 @@ function prevPage() {
     change(current_page);
   }
 }
+
+// boton next
 function nextPage() {
   containerCard.innerHTML = "";
   if (current_page < totNumPages()) {
@@ -40,162 +42,173 @@ function nextPage() {
     change(current_page);
   }
 }
+
+
+// crear pagina (funcion)
 function change(page) {
-  var btn_next = document.getElementById("btn_next");
+  const btn_next = document.getElementById("btn_next");
   btn_next.addEventListener("click", nextPage);
-  var btn_prev = document.getElementById("btn_prev");
+  const btn_prev = document.getElementById("btn_prev");
   btn_prev.addEventListener("click", prevPage);
 
-  var page_span = document.getElementById("page");
+  const page_span = document.getElementById("page");
   if (page < 1) page = 1;
   if (page > totNumPages()) page = totNumPages();
 
-  for (var i = (page - 1) * obj_per_page; i < page * obj_per_page; i++) {
+  for (let i = (page - 1) * obj_per_page; i < page * obj_per_page; i++) {
     
-    let pokemon_unit = data.pokemon[i];
-    let typePokemon = getTypePokemon(pokemon_unit.type);
-
-    containerCard.innerHTML += ` 
-      <div class="card-pokemon"> 
-        <button class="card" id="${Math.abs(pokemon_unit.num)-1}">  
-          <div class="card-content">
-            <p class="numberPokemon" >${pokemon_unit.num}</p>
-            <h3>${pokemon_unit.name}</h3>
-            <div class="power">
-            ${typePokemon}
-            </div>
-          </div>
-          <div class="card-img">
-            <img src="${pokemon_unit.img}" class="imagen" alt=""> 
-          </div>
-        </button>
-      </div>`;
-        
+    containerCard.innerHTML +=  card(i,data.pokemon[i]);
+    // crea funciones para modales
+    openCard(data.pokemon, body)
   }
 
   for(let i = 0; i < containerCard.children.length; i++){
-    console.log(containerCard.children[i]);
-    containerCard.children[i].addEventListener("click", (e) => {
-      console.log(e.target.classList(card-pokemon))
-
+    containerCard.children[i].addEventListener("click", () => {
     });
   }
 
   page_span.innerHTML = page;
-  if (page == 1) {
+  if (page === 1) {
     btn_prev.style.visibility = "hidden";
   } else {
     btn_prev.style.visibility = "visible";
   }
-  if (page == totNumPages()) {
+  if (page === totNumPages()) {
     btn_next.style.visibility = "hidden";
     
   } else {
     btn_next.style.visibility = "visible";
   }
 }
+
+
+// crear pagina (ejecucion)
 window.onload = function () {
   change(1);
 };
 
 
+// interaccion input busqueda
+    // EVALUAR SI LO Q ENTRA EN INPUT EXISTE COMO PARTE DE ALGUN NOMBRE
+    // poke.name   (el nombre en objeto)
+    // includes    (revisa si incluye en )
+    // val         (el texto q esta entrando en input)
 
-function openModal(idPokemon) {
-  infoCard.style.display = "block";
-  console.log(idPokemon); 
-  const objPokemon = data.pokemon[idPokemon];
-  console.log(objPokemon);
-  document.getElementById("infoCard").innerHTML = `
-        <div class="mainInfo">
-          <p class="numberPokemon">${objPokemon.num}</p>
-          <p class="close"></p>
-          <h3>${objPokemon.name}</h3>
-          <img src="${objPokemon.img}" alt="">
-          <div>
-            <div class="power">${objPokemon.type}</div>
-            <h4>Sobre</h4>
-            <div>
-              <div class="weightDiv">
-                <div>            
-                  <img src="" alt="">
-                  <p class="weight"></p>
-                </div>
-                <p>Peso</p>
-              </div>
-              <div class="sizeDiv">
-                <div>            
-                  <img src="" alt="">
-                  <p class="size"></p>
-                </div>
-                <p>Medida</p>
-              </div>
-              <div class="movesDiv">
-                <p class="moves"></p>
-                <p>Moves</p>
-            </div>
-            <div class="description">
-              <p>${objPokemon.about}</p>
-            </div>
-            <div>
-              <h3>Estadística</h3>
-              <div>
+buscar.addEventListener("keydown", e => {
+  const val = e.target.value.toLowerCase()
+  let out = ''
+  data.pokemon.forEach((poke,idx)=>{
     
-              </div>
-            </div>
-          </div> `;         
-}
+    if(poke.name.includes(val)){
+      out += card(idx, poke)
+    }
+  })
+  containerCard.innerHTML = out
+
+  // crea funciones para modales
+  openCard(data.pokemon, body)
+});
 
 
 
-document.addEventListener("keyup", e => {
- if(e.target.matches(".search")){
-  
-    document.querySelectorAll(".card").forEach(nombre =>{
-    nombre.textContent.toLowerCase().includes(e.target.value) // incluye el valor 
-    ?nombre.classList.remove()          //remueve elementos del dom 
-    :nombre.classList.add("filtro")
+// interaccion botones filtrado
+
+buttonType.addEventListener("click", e=>{
+  // como el div q contiene el boton ----> NODO PADRE
+  const parent = e.target.parentNode
+  // insertAdjacentHTML   --->   AGREGA CONTENIDO SIN RECARGAR EL DIV a diferencia de innerHTML
+  parent.insertAdjacentHTML(
+    'beforeend', `
+    <ul class="list-type" id="listType">
+      <li class="btn-filter-type water"    poke-type="water"    >  Water     </li>
+      <li class="btn-filter-type dragon"   poke-type="dragon"   >  Dragon    </li>
+      <li class="btn-filter-type fire"     poke-type="fire"     >  Fire      </li>
+      <li class="btn-filter-type electric" poke-type="electric" >  Electric  </li>
+      <li class="btn-filter-type fairy"    poke-type="fairy"    >  Fairy     </li>
+      <li class="btn-filter-type ghost"    poke-type="ghost"    >  Ghost     </li>
+      <li class="btn-filter-type ice"      poke-type="ice"      >  Ice       </li>
+      <li class="btn-filter-type normal"   poke-type="normal"   >  Normal    </li>
+      <li class="btn-filter-type grass"    poke-type="grass"    >  Grass     </li>
+      <li class="btn-filter-type fighting" poke-type="fighting" >  Fighting  </li>
+      <li class="btn-filter-type bug"      poke-type="bug"      >  Bug       </li>
+      <li class="btn-filter-type poison"   poke-type="poison"   >  Poison    </li>
+      <li class="btn-filter-type flying"   poke-type="flying"   >  Flying    </li>
+      <li class="btn-filter-type psychic"  poke-type="psychic"  >  Psychic   </li>
+      <li class="btn-filter-type rock"     poke-type="rock"     >  Rock      </li>
+      <li class="btn-filter-type dark"     poke-type="dark"     >  Dark      </li>
+      <li class="btn-filter-type ground"   poke-type="ground"   >  Ground    </li>
+      <li class="btn-filter-type steel"    poke-type="steel"    >  Steel     </li>
+    </ul>
+    `
+  );
+
+  // tomar todos los botones
+  const btnFilterType = document.querySelectorAll('.btn-filter-type')
+
+  // filtrar y crear modales(openCard)
+  btnFilterType.forEach(btn => {
+    btn.addEventListener('click', btn_e=>{
+      //pokeType  --->  el tipo en el atributo del boton
+      const pokeType = btn_e.target.getAttribute('poke-type')
+      let out = ''
+      
+      // filtrado
+      data.pokemon.forEach((e_poke,idx_poke)=>{
+        // e_poke.type (es el arregle de tipos de pokemon en el objeto con 251)
+        //.includes    (si está incluido)
+        // pokeType    (el tipo en el atributo del boton)
+        if(e_poke.type.includes(pokeType)){
+          out += card(idx_poke, e_poke)
+        }
+      })
+
+      containerCard.innerHTML = out
+
+      // modal
+      openCard(data.pokemon, body) 
+    })
   });
-  if(e.key === "Escape")e.target.value = "";
-}
-  
-});
 
 
-/*
-const buttonType = document.getElementById("buttomType");
-const listType = document.getElementById("listType");
+  // mostrar y ocultar lista de botones azules
+  const listType = document.querySelector('#listType')
+  // escucha cuando entra
+  listType.addEventListener('mouseenter', ()=>{
+    // escucha cuando sale
+    listType.addEventListener('mouseleave', ()=>{
+      // remueve
+      listType.remove()
+    })
+  })
 
-//const iter = data.pokemon.filter(i=> type ="water")
 
-
-
-buttonType.addEventListener("click", ()=>{
-  listType.style.display = "block";
-});
-listType.addEventListener("click",(e)=>{
-const classListType = e.target.classList[0];
-switch (classListType) {
-case ('water'):  containerCard.innerHTML += ` 
-<div id="card-pokemon>
-    <button class="card" id="${Math.abs(pokemon_unit.num)-1}">
-      <div class="card-content">
-        <p class="numberPokemon" >${pokemon_unit.num}</p>
-        <h3>${pokemon_unit.name}</h3>
-        <div class="power">
-        ${typePokemon}
-        </div>
-      </div>
-      <div class="card-img">
-        <img src="${pokemon_unit.img}" class="imagen" alt=""> 
-      </div>
-    </button>
-  </div>`;
 })
-  
 
-    break;
 
-  default:
-    break;
-}
-});*/
+// interaccion bonton mostrar todos
+
+buttonOrder.addEventListener("click", ()=>{
+  // crea todas las card
+  let out = ''
+  data.pokemon.forEach((e_poke,idx_poke)=>{
+    out += card(idx_poke, e_poke)
+  })
+  containerCard.innerHTML = out
+  // crea funciones para modales
+  openCard(data.pokemon, body)
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
